cf8725bd3db00148dfd214b8bbdf27c844c3602f
HawkarShakhawan <rozhgarshaxawan77@gmail.com>
2024-04-05 23:24:49 +0300

    admin folder

---
+++ admin/admin_header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/header_style.css">
+</head>
+<body>
+    <header>
+    <nav>
+    <ul>
+        <li><a href="home.php">Home</a></li>
+        <li class="dropdown">
+            <a href="javascript:void(0)" class="dropbtn">Categories</a>
+            <div class="dropdown-content">
+                <a href="#">Category 1</a>
+                <a href="#">Category 2</a>
+                <a href="#">Category 3</a>
+            </div>
+        </li>
+        <li class="search-container">
+            <form action="">
+                <input type="text" placeholder="Search.." name="search">
+                <button type="submit">
+                    <i class="fa fa-search"></i> <!-- This is the search icon -->
+                </button>
+            </form>
+        </li>
+        <li><a href="authors.php">Authors</a></li>
+        <li><a href="dashboard.php">Dashboard</a></li>
+        <li><a href="messages.php">Messages</a></li>
+        <li><a href="about.php">About</a></li>
+  
+
+     
+
+    </ul>
+</nav>
+    </header>
+</body>
+</html>
+
